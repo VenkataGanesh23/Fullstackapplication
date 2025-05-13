a85d1fd14b1b09fdcdf4f0cc86ba321232e4680b
Ganith <ramireddy.ganith@gmail.com>
2025-05-13 13:05:52 +0530

    changed the file structure

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,7 @@
 import { ApolloServer } from 'apollo-server';
-import { typeDefs } from './schema';
-import { resolvers } from './resolver';
-import { createContext } from './context';
+import { typeDefs } from './src/schema';
+import { resolvers } from './src/resolver';
+import { createContext } from './src/context';
 
 const server = new ApolloServer({ typeDefs, resolvers, context: createContext });
 server.listen({ port: 4001 }).then(({ url }) => {
